5c06687e921bfb38b035210f7bdf23d026e0d43d
mhsnzkn <muhsinozkan12@gmail.com>
2021-07-22 14:33:39 +0100

    datatables export buttons added, Headers editted

---
+++ Data/Entities/OrderPersonnel.cs
@@ -0,0 +1,18 @@
+﻿using Core.Data;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace Data.Entities
+{
+    public class OrderPersonnel : Entity
+    {
+        public Order Order { get; set; }
+        public int OrderId { get; set; }
+        public Personnel Personnel { get; set; }
+        public int PersonnelId { get; set; }
+
+    }
+}
