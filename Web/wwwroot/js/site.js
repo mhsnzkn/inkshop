2b3188b54debee26d2ba28e1fafdf4f47b94aeba
mhsnzkn <muhsinozkan12@gmail.com>
2021-07-29 01:11:23 +0100

    usermessage added to ajax call

--- Web/wwwroot/js/site.js
+++ Web/wwwroot/js/site.js
@@ -8,11 +8,11 @@ function ajaxSuccess(xhr, returnUrl) {
     if (xhr.responseJSON.error === false)
         window.location.href = returnUrl;
     else {
-        alertify.error("Kayit Basarisiz");
+        alertify.error(xhr.responseJSON.userMessage);
         console.log(xhr.responseJSON.message);
     }
 }
 function ajaxFail(err) {
-    alertify.error("Baglanti Hatasi");
+    alertify.error("Bağlantı Hatası");
     console.log(err);
 }
