defb7ec2d996c957431e771bf968780a42b631dd
mhsnzkn <muhsinozkan12@gmail.com>
2021-09-01 12:15:43 +0100

    Personnel Commision Precision increased to 4

---
+++ Data/Migrations/20210901105440_PersonnelCommisionPrecission.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Data.Migrations
+{
+    public partial class PersonnelCommisionPrecission : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Commission",
+                table: "Personnel",
+                type: "decimal(7,4)",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(5,2)");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Commission",
+                table: "Personnel",
+                type: "decimal(5,2)",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(7,4)");
+        }
+    }
+}
